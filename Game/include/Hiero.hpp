1a84b529d0da0e4dc2e409d766c67bd40351e166
Daniel Marcos <wisebdan@gmail.com>
2017-09-28 14:46:48 -0300

    Acrescenta atributos e funções básicas ao Header

--- Game/include/Hiero.hpp
+++ Game/include/Hiero.hpp
@@ -1,8 +1,25 @@
 #ifndef __HIERO_HPP__
 #define __HIERO_HPP__
 
-class Hiero{
-     
+#include "GameObject.hpp"
+#include "Sprite.hpp"
+
+#include <string>
+
+class Hiero : public GameObject {
+     public:
+          Hiero();
+		~Hiero();
+		void Update(float);
+		void Render(void);
+		bool IsDead(void);
+		void RequestDelete(void);
+		void NotifyCollision(GameObject);
+          bool Is(string);
+		Rect GetWorldRenderedRect(void) const;
+          
+	private:
+		Sprite sp;
 };
 
 #endif // __HIERO_HPP__
